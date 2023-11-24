e92898ce814f4b160dec73b41ef58d5e90eccfc6
Kiran Kannur <kiran.kannur72@gmail.com>
2023-11-25 01:58:15 +0530

    added about section - nov 25th

--- main.go
+++ main.go
@@ -16,19 +16,36 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("./src/template/index.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// fmt.Println("Hello World !!")
-		err = tmpl.Execute(w, "Kiran")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+	router.HandleFunc("/", handleHome)
+	router.HandleFunc("/about", handleAbout)
 
-	})
 	fmt.Printf("Listening at http://localhost:%s ", port[1:])
 	http.ListenAndServe(port, router)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./src/template/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, "Kiran")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("./src/template/about.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, "Kiran Kannur")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
